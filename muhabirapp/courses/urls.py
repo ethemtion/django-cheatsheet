bc8efbac04fafbab84dc3df9436ef445c20469e2
ethem <ethem.topcu13@hotmail.com>
2023-08-03 01:29:34 +0300

    UrlsveViews

--- muhabirapp/courses/urls.py
+++ muhabirapp/courses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-
+##Sıralama önemli.
 urlpatterns = [
     path("", views.kurslar),
     path("list", views.kurslar),
-    path("details", views.details),
-    path("programlama", views.programlama),
-    path("mobil-uygulamalar", views.mobiluygulamalar),
+    path("<kurs_adi>", views.details),
+    path('kategori/<int:category_id>', views.getCoursesByCategoryId),
+    path('kategori/<str:category_name>', views.getCoursesByCategoryName, name='courses_by_category'),
+    
 ]
--- muhabirapp/courses/views.py
+++ muhabirapp/courses/views.py
@@ -1,17 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
 
+data = {
+    "programlama": "Programlama kategorisine ait kurslar",
+    "web-gelistirme": "Web gelistirme kategorisine ait kurslar",
+    "mobil": "Mobil kategorisine ait kurslar",
+}
+
+
 def kurslar(request):
     return HttpResponse('Kurs Listesi')
 
-def programlama(request):
-    return HttpResponse('Programlama kurs Listesi')
+def details(request, kurs_adi):
+    return HttpResponse(f'{kurs_adi} detay sayfası')
 
-def mobiluygulamalar(request):
-    return HttpResponse('Mobil uygulama kurs Listesi')
+def getCoursesByCategoryName(request, category_name):
+    try:
+        category_text = data[category_name]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("Yanlış kategori seçimi")
+        
 
-def details(request):
-    return HttpResponse('Kurs detay sayfası')
+
+def getCoursesByCategoryId(request, category_id):
+    # return HttpResponse(category_id)
+
+    category_list = list(data.keys())
+    try:
+        category_name = category_list[category_id-1]
+
+        redirect_url = reverse('courses_by_category', args=[category_name])
+        return redirect(redirect_url)
+    except:
+        return HttpResponseNotFound("S")  
