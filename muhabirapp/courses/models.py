d7486e69dbbc032ddfdb2090805eb4e2167575c6
ethem <ethem.topcu13@hotmail.com>
2023-08-17 02:14:12 +0300

    dataset connections 1toM

--- muhabirapp/courses/models.py
+++ muhabirapp/courses/models.py
@@ -4,13 +4,27 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50, blank=True, editable=False, null=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(args, kwargs)
+
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     imageUrl = models.CharField(max_length=50, blank=False)
-    date =  models.DateField()#auto_now olabilirdi
+    date =  models.DateField(auto_now=True)#auto_now olabilirdi
     isActive = models.BooleanField()
     slug = models.SlugField(default="", blank=True, editable=False, null=False, unique=True, db_index=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
+
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -21,14 +35,3 @@ class Course(models.Model):
         return f"{self.title} {self.date}"
     
 
-class Categories(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True, editable=False, null=False, unique=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(args, kwargs)
-
-
-    def __str__(self):
-        return f"{self.name}"
